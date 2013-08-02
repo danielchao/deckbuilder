2b7e16d47caa04185e6b34e1b1625638363d426e
dcmonger <unexrogh@gmail.com>
2013-08-02 09:24:38 -0700

    card associations

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,20 +1,25 @@
 class DecksController < ApplicationController
     before_action :correct_user, only: [:destroy, :create]
+
     def show
         @deck = Deck.find(params[:id])
     end
 
     def create
+        card_regex = /^([0-9]+)\s+([^\s][0-9a-zA-Z,\-\' \/]*)$/
         @deck = current_user.decks.build(deck_params)
         cards = []
         @cards = params[:cards][:names].each_line do |line| 
             cards.push(line.strip) if !line.strip.blank?
         end
-        #cards.each do |card|
-            #@deck.cards.create(name: card)
-        #end
-        #render text: cards
-        
+        cards.each do |card|
+            if m = card_regex.match(card)
+                (1..m[1].to_i).each do |n|
+                    c = Card.find_or_create_by(name: m[2])
+                    @deck.cards.push(c)
+                end
+            end
+        end
         if @deck.save
             redirect_to user_path(current_user.id), notice: "Success!"
         else
@@ -28,12 +33,17 @@ class DecksController < ApplicationController
 
     def destroy
         @deck = Deck.find(params[:id])
+        cards = @deck.cards
         @deck.destroy
+        cards.each do |card|
+            card.destroy if card.decks.count == 0
+        end
         redirect_to user_path(current_user.id)
     end
 
     def show
         @deck = Deck.find(params[:id])
+        @cards = @deck.cards.group('name')
     end
 
     private
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,6 +1,6 @@
 class Deck < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
-  has_many :deck_cards 
+  has_many :deck_cards
   has_many :cards, through: :deck_cards
 end
